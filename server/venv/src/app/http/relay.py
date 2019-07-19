3f8dc7d76e791dcf75be63762603b2058f4de1ea
HeJingjing <campanulamediuml@gmail.com>
2019-07-20 00:23:45 +0800

    引入分词初始化

--- server/venv/src/app/http/relay.py
+++ server/venv/src/app/http/relay.py
@@ -2,7 +2,7 @@ import jieba
 class Relay(object):
     server = None
 
-    @staticmethod
     def init(server):
         Relay.server = server
+        _initial_split_word = list(jieba.cut_for_search('服务器操作'))
 # 服务器核心操作放在这里
